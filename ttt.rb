e6a01f877218b929ec140e5d53b15473e6441a67
Endand <endand2898@gmail.com>
2024-03-09 19:52:10 -0500

    Added play_game method in TicTacToe class
    Now it switches player based on turn

--- ttt.rb
+++ ttt.rb
@@ -1,7 +1,13 @@
 class GameBoard
   def initialize
     puts 'Game Board created'
+    #makes 2D gameboard
   end
+
+  #method to display gameboard
+  #method to mark a cell
+  #method to check for a win
+  
 end
 
 class Player
@@ -17,18 +23,44 @@ class Player
   def symbol
     @symbol
   end
+
+  #method for making a move
 end
 
 class TicTacToe
+  #manage overall game flow
+  #manage player turn
+  #check for win or draw
   def initialize
     puts "Play Tic Tac Toe!\n\n"
 
     print 'Enter Player1 Name: '
-    player1 = Player.new(gets.chomp.capitalize, 'O')
+    @player1 = Player.new(gets.chomp.capitalize, 'O')
     print 'Enter Player2 Name: '
-    player2 = Player.new(gets.chomp.capitalize, 'X')
+    @player2 = Player.new(gets.chomp.capitalize, 'X')
 
-    puts "\nWelcome #{player1.name} and #{player2.name}!"
+    puts "\nWelcome #{@player1.name} and #{@player2.name}!"
+
+    play_game
+  end
+
+  def play_game
+    game_result=''
+    turn=0
+    while game_result != 'win' and game_result!='draw'
+    
+      player = (turn.even? ? @player1 : @player2)
+
+      p player.symbol
+
+      if turn==4 
+        game_result='win'
+      end 
+
+      turn+=1
+
+    end
+
   end
 end
 
